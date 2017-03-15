712d0aa6a0a472e4d0ed846255a6d4ae40e568fb
Alejandro <agr0095@alu.ubu.es>
2017-03-15 19:52:04 +0100

    Bug fix

--- preproc/preprocess_data.py
+++ preproc/preprocess_data.py
@@ -1,12 +1,10 @@
-import itertools
-import os
-import pickle
-import re
-
 import nltk
+import itertools
 import numpy as np
-
-from preproc.load_data import load_cornell, load_cornell_from_file, load_simpsons_from_file,load_simpsons
+import pickle
+from load_data import load_cornell, load_cornell_from_file, load_simpsons_from_file,load_simpsons
+import os
+import re
 
 WHITELIST = '0123456789abcdefghijklmnopqrstuvwxyz .?!' # space is included in whitelist
 
@@ -24,10 +22,8 @@ path_to_variables = "./variables/"
 
 _WORD_SPLIT = re.compile("([.,!?\"':;)(])")
 
-'''
-Given a sentence, returns a list of tokens (each word in a token).
-'''
 def basic_tokenizer(sentence):
+  """Very basic tokenizer: split the sentence into a list of tokens."""
   words = []
   if isinstance(sentence, float):   # In case the sentence is just a number.
       sentence = str(sentence)
@@ -36,46 +32,36 @@ def basic_tokenizer(sentence):
   return [w for w in words if w]
 
 '''
-    Given a set of sentences, it converts them all to lowercase, deletes invalid characters and tokenize.
-    Returns the set after this operations has been performed.
+ remove anything that isn't in the vocabulary
+    return str(pure ta/en)
 '''
-def basic_preproc(data):
-    # All sentences to lower case
-    q_simp = [line.lower() for line in data]
-    # Eliminate characters that are not in the white list.
-    q_simp = [ filter_line(line) for line in q_simp ]
-    q_simp_tok = [basic_tokenizer(line) for line in q_simp]
-    return q_simp_tok
+def filter_line(line):
+    return ''.join([ ch for ch in line if ch in WHITELIST ])
 
 '''
-Given a sentence, it deletes all the characters that are not in the WHITELIST.
+ filter too long and too short sequences
+    return tuple( filtered_ta, filtered_en )
 '''
-def filter_line(sentence):
-    return ''.join([ ch for ch in sentence if ch in WHITELIST ])
-
-'''
-Given a set of questions and answers, it deletes all those pairs where at least one of the sentences does not
-fulfill the length requeriments.
-'''
-def filter_data(questions, answers):
+def filter_data(sequences):
     q_filt = []
     a_filt = []
-    for i in range(0, len(questions)):
-        length1 = len(questions[i])
-        length2 = len(answers[i])
+    for i in range(0, int(sequences.size/2)):
+        length1 = len(sequences[i])
+        length2 = len(sequences[i*2])
         if length1 >= limit['minq'] and length1 <= limit['maxq']:
             if length2 >= limit['mina'] and length2 <= limit['maxa']:
-                q_filt.append(questions[i])
-                a_filt.append(answers[i])
+                q_filt.append(sequences[i])
+                a_filt.append(sequences[i*2])
 
     return q_filt,a_filt
 
 
 '''
-Given a set of questions and answers (already tokenized), it converts words to indexes and adds a zero-padding if
-necessary.
+ create the final dataset :
+  - convert list of items to arrays of indices
+  - add zero padding
+      return ( [array_en([indices]), array_ta([indices]) )
 
-Returns one set of questions and another one of answers ater being processed.
 '''
 
 
@@ -99,9 +85,10 @@ def zero_pad(qtokenized, atokenized, w2idx):
 
 
 '''
-Given a sequence, a lookup table and the expected length of the result, it replaces laces words with indices.
+ replace words with indices in a sequence
+  replace with unknown if word not in lookup
+    return [list of indices]
 
-Returns a list of indexes that form the sequence.
 '''
 
 def pad_seq(seq, lookup, maxlen):
@@ -110,13 +97,13 @@ def pad_seq(seq, lookup, maxlen):
         if word in lookup:
             indices.append(lookup[word])
         else:
-            indices.append(lookup[UNK]) # If the word is not in the vocabulary
+            indices.append(lookup[UNK])
     return indices + [0] * (maxlen - len(seq))
 
 '''
-Given a set of sentences, it gets the VOCAB_SIZE more common words and creates structures to easily map words to their ID.
-
-Returns dictionaries index2word and word2index to map from word to index and viceversa.
+ Read list of words, create index to word,
+  word to index dictionaries
+    return tuple( vocab->(word, count), idx2w, w2idx )
 '''
 def index_(tokenized_sentences, freq_dist = None):
     # get frequency distribution
@@ -131,15 +118,6 @@ def index_(tokenized_sentences, freq_dist = None):
     return index2word, word2index, freq_dist
 
 
-'''
-Prepares the data before we can feed it to the Seq2Seq model.
-
-It first loads the datasets and performs a basic preprocessing on their sentences. Then it selects and prepares all
-those sentences that can be used in our Seq2Seq model.
-
-Stores the final result to disk.
-
-'''
 def preprocess_data():
 
     print("Loading The Simpsons Dataset...")
@@ -149,11 +127,13 @@ def preprocess_data():
     except FileNotFoundError:
         data_simp = load_simpsons(output_file=True)
 
-    print("Basic preprocessing The Simpsons Dataset...")
+    print("Preprocessing The Simpsons Dataset...")
 
     q_simp = data_simp.question
-
-    # Repeat steps with the answers
+    # All sentences to lower case
+    q_simp = [line.lower() for line in q_simp]
+    # Eliminate characters that are not in the white list.
+    q_simp = [ filter_line(line) for line in q_simp ]
     a_simp = data_simp.answer
     a_simp = [line.lower() for line in a_simp]
     a_simp = [ filter_line(line) for line in a_simp ]
@@ -168,7 +148,7 @@ def preprocess_data():
     except FileNotFoundError:
         data_corn = load_cornell(output_file=True)
 
-    print("Basic preprocessing the Cornell Movies Dataset...")
+    print("Preprocessing the Cornell Movies Dataset...")
 
     q_corn = data_corn.question
     q_corn = [str(line).lower() for line in q_corn]
@@ -182,9 +162,9 @@ def preprocess_data():
 
     # Filter by size
     print("Filtering sentences by size...")
-    q_simp_filt, a_simp_filt = filter_data(q_simp_tok, a_simp_tok)
+    q_simp_filt, a_simp_filt = filter_data(all_simp_tok)
     all_simp_filt = np.concatenate((np.array(q_simp_filt), np.array(a_simp_filt)), axis=0)
-    q_corn_filt, a_corn_filt = filter_data(q_corn_tok, a_corn_tok)
+    q_corn_filt, a_corn_filt = filter_data(all_corn_tok)
     all_corn_filt = np.concatenate((np.array(q_corn_filt), np.array(a_corn_filt)), axis=0)
     all_filt = np.concatenate((np.array(all_corn_filt), np.array(all_simp_filt)), axis=0)
 
@@ -220,7 +200,3 @@ def preprocess_data():
     # write to disk : data control dictionaries
     with open(path_to_variables+'metadata.pkl', 'wb') as f:
         pickle.dump(metadata, f)
-
-    return idx_simp_q, idx_simp_a, idx_corn_q, idx_corn_a, metadata
-
-preprocess_data()
